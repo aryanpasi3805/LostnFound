1d0101a88336808de7d126b36259069fab96bea5
aryanpasi3805 <159549382+aryanpasi3805@users.noreply.github.com>
2026-04-09 15:19:20 +0000

    wait

--- backend/routes/claimRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createClaim, getClaims } = require("../controllers/claimController");
-
-router.route("/").get(getClaims).post(createClaim);
-
-module.exports = router; 
